cee9877b9d44bc2627e348acf8d95ddfcccbec33
Eduard Nikolaiev <e.nikolaiev@astoundcommerce.com>
2019-03-02 17:36:59 +0200

    Chasing nearby enemies

--- battlecity-servers/src/main/java/com/codenjoy/dojo/battlecity/client/YourSolver.java
+++ battlecity-servers/src/main/java/com/codenjoy/dojo/battlecity/client/YourSolver.java
@@ -22,9 +22,14 @@ package com.codenjoy.dojo.battlecity.client;
  * #L%
  */
 
+import com.codenjoy.dojo.battlecity.model.BoardLee;
+import com.codenjoy.dojo.battlecity.model.Elements;
+import com.codenjoy.dojo.battlecity.model.PointLee;
 import com.codenjoy.dojo.services.*;
 import com.codenjoy.dojo.client.Solver;
 import com.codenjoy.dojo.client.WebSocketRunner;
+
+import java.util.*;
 
 /**
  * User: your name
@@ -44,9 +49,17 @@ public class YourSolver implements Solver<Board> {
         this.board = board;
         if (board.isGameOver()) return "";
 
-        String move = moveDir(board);
+//        PointLee dest = new PointLee(21,21);
+        char[][] field = board.getField();
+        int sizeY = field[0].length;
+        Point destPoint = getClosestEnemy(board);
+        PointLee dest = new PointLee(destPoint.getX(), invertVertical(destPoint.getY(), sizeY));
 
-        return move + ',' + Direction.ACT.toString();
+
+        return getDirectionToDestination(board, dest).toString() + ',' + Direction.ACT.toString();
+//        String move = moveDir(board);
+//
+//        return move + ',' + Direction.ACT.toString();
     }
 
     public static void main(String[] args) {
@@ -56,24 +69,57 @@ public class YourSolver implements Solver<Board> {
                 new YourSolver(new RandomDice()),
                 new Board());
     }
+    
+    private Direction getDirectionToDestination(Board board, PointLee dest) {
+        char[][] field = board.getField();
+        int sizeX = field.length;
+        int sizeY = field[0].length;
+        BoardLee boardLee = new BoardLee(sizeX, sizeY);
+        List<Point> barriers = board.getBarriers();
+//        List<Point> enemies = board.getEnemies();
+        barriers.forEach(p -> boardLee.setObstacle(p.getX(), invertVertical(p.getY(), sizeY)));
+//        enemies.forEach(p -> boardLee.setObstacle(p.getX(), invertVertical(p.getY(), sizeY)));
+        Point me = board.getMe();
+        PointLee src = new PointLee(me.getX(), invertVertical(me.getY(), sizeY));
+        Optional<List<PointLee>> solution = boardLee.trace(src, dest);
+
+        if (solution.isPresent()) {
+            List<PointLee> path = solution.get();
+            PointLee p = path.stream().skip(1).findFirst().get();
+            System.out.printf("ME: x:%2d, y:%2d\n", me.getX(), me.getY());
+            int to_x = p.x();
+            int to_y = invertVertical(p.y(), sizeY);
+            System.out.printf("TO: x:%2d, y:%2d\n", to_x, to_y);
+            if (to_y < me.getY()) return Direction.DOWN;
+            if (to_y > me.getY()) return Direction.UP;
+            if (to_x < me.getX()) return Direction.LEFT;
+            if (to_x > me.getX()) return Direction.RIGHT;
+        }
+        return Direction.ACT;
+    }
+
+    private Point getClosestEnemy(Board board) {
+        List<Point> enemies = board.getEnemies();
+        Point me = board.getMe();
+        return enemies.stream()
+                .min(Comparator.comparing(p -> Math.round(p.distance(me))))
+                .get();
+    }
+
+    private int invertVertical(int val, int dimY) {
+        return dimY - val - 1;
+    }
 
     private String moveDir(Board board) {
-        int meX = board.getMe().getX();
-        int meY = board.getMe().getY();
-        Direction move = Direction.DOWN;
+        Direction move;
 
-        if (!isBarrierAhead(board)) {
-            move = currentDirection;
+        if (enemyNeighbourDirection(board) != null) {
+            move = Direction.ACT;
         } else {
-            // find empty cell
-            if (!board.isBarrierAt(meX + 1, meY)) {
-                move = Direction.RIGHT;
-            } else if (!board.isBarrierAt(meX, meY + 1)) {
-                move = Direction.UP;
-            } else if (!board.isBarrierAt(meX - 1, meY)) {
-                move = Direction.LEFT;
-            } else if (!board.isBarrierAt(meX, meY - 1)) {
-                move = Direction.DOWN;
+            if (!isBarrierAhead(board)) {
+                move = currentDirection;
+            } else {
+                move = findAvailableDirection(board);
             }
         }
 
@@ -82,10 +128,67 @@ public class YourSolver implements Solver<Board> {
     }
 
     private boolean isBarrierAhead(Board board) {
-        int meX = board.getMe().getX();
-        int meY = board.getMe().getY();
-        Point point = new PointImpl(meX, meY);
+        Point point = getCurrentMePoint(board);
         Point potentialMove = currentDirection.change(point);
         return board.isBarrierAt(potentialMove.getX(), potentialMove.getY());
+    }
+
+    private Direction findAvailableDirection(Board board) {
+        Point point = getCurrentMePoint(board);
+        Direction randomDirection = Direction.UP;
+
+        for (int i = 0; i < 100; i++) {
+            randomDirection = Direction.random();
+            Point potentialMove = randomDirection.change(point);
+
+            if (!board.isBarrierAt(potentialMove.getX(), potentialMove.getY())) {
+                return randomDirection;
+            }
+        }
+
+        // default
+        return randomDirection;
+    }
+
+    private Direction enemyNeighbourDirection(Board board) {
+        Point point = getCurrentMePoint(board);
+        int x = point.getX();
+        int y = point.getY();
+
+        Direction direction = null;
+        List<Elements> neighbourCells = board.getNear(point);
+
+        List<Elements> enemies = new ArrayList<Elements>(
+            Arrays.asList(
+                Elements.AI_TANK_UP,
+                Elements.AI_TANK_DOWN,
+                Elements.AI_TANK_LEFT,
+                Elements.AI_TANK_RIGHT,
+                Elements.OTHER_TANK_UP,
+                Elements.OTHER_TANK_DOWN,
+                Elements.OTHER_TANK_LEFT,
+                Elements.OTHER_TANK_RIGHT
+            )
+        );
+
+        enemies.forEach(enemy -> {
+            if (board.isAt(x, y, enemy))
+            return;
+        });
+
+
+        return direction;
+    }
+
+    private Point nearbyEnemyLocation() {
+        int range = 5;
+        Point point = PointImpl.pt(0, 0);
+        return point;
+    }
+
+    private Point getCurrentMePoint(Board board) {
+        int meX = board.getMe().getX();
+        int meY = board.getMe().getY();
+        return PointImpl.pt(meX, meY);
     }
 }
